7e2c0643de19d5e49639a7be5218e70f3c3cdc20
ednukros <isanmenarguez@gmail.com>
2022-12-25 23:38:12 +0100

    Iteración #2: Destructuring

--- es6-1.js
+++ es6-1.js
@@ -2,8 +2,6 @@
 // que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
 // por consola la suma de los dos parametros.
 
-
-
 // 1.1 Ejecuta esta función sin pasar ningún parametro
 
 const arrowFunction = (a = 10, b = 5) => {
@@ -17,3 +15,47 @@ arrowFunction(1);
 // 1.3 Ejecuta esta función pasando dos parametros
 
 arrowFunction(2,5);
+
+// 2.1 En base al siguiente javascript, crea variables en base a las propiedades 
+// del objeto usando object destructuring e imprimelas por consola. Cuidado, 
+// no hace falta hacer destructuring del array, solo del objeto.
+
+ const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
+
+ let {title, gender, year} = game;
+ console.log(title,gender,year);
+
+// 2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
+// llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
+// imprimelo por consola.
+
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+let [fruit1, fruit2, fruit3] = fruits;
+
+console.log(fruit1,fruit2,fruit3);
+
+// 2.3 En base al siguiente javascript, usa destructuring para crear 2 
+// variables igualandolo a la función e imprimiendolo por consola.
+
+ const animalFunction = () => {
+    return {name: 'Bengal Tiger', race: 'Tiger'}
+};
+
+let {name, race} = animalFunction();
+console.log(name,race);
+// 2.4 En base al siguiente javascript, usa destructuring para crear las 
+// variables name y itv con sus respectivos valores. Posteriormente crea 
+// 3 variables usando igualmente el destructuring para cada uno de los años 
+// y comprueba que todo esta bien imprimiendolo.
+
+ const car = {name2: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+ let {name2, itv} = car;
+console.log(name2,itv);
+
+
+let [a,b,c] = car.itv;
+
+console.log(a,b,c);
+
