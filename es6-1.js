e11ffcafaeffb0ece909631de1c5d65c2c675707
ednukros <isanmenarguez@gmail.com>
2023-01-11 17:35:19 +0100

    iteration 7

--- es6-1.js
+++ es6-1.js
@@ -439,3 +439,77 @@ alienMutated.mutation = {
 	...mutation
 }
 console.log(alienMutated);
+
+// 7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+// los alumnos usando la función .reduce().
+
+const exams = [{
+		name: 'Yuyu Cabeza Crack',
+		score: 5
+	},
+	{
+		name: 'Maria Aranda Jimenez',
+		score: 1
+	},
+	{
+		name: 'Cristóbal Martínez Lorenzo',
+		score: 6
+	},
+	{
+		name: 'Mercedez Regrera Brito',
+		score: 7
+	},
+	{
+		name: 'Pamela Anderson',
+		score: 3
+	},
+	{
+		name: 'Enrique Perez Lijó',
+		score: 6
+	},
+	{
+		name: 'Pedro Benitez Pacheco',
+		score: 8
+	},
+	{
+		name: 'Ayumi Hamasaki',
+		score: 4
+	},
+	{
+		name: 'Robert Kiyosaki',
+		score: 2
+	},
+	{
+		name: 'Keanu Reeves',
+		score: 10
+	}
+];
+
+
+const sum = exams.reduce(function (acc, exam) {
+	return acc + exam.score
+}, 0);
+
+console.log(sum);
+
+// 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+// alumnos que esten aprobados usando la función .reduce().
+
+const approved = exams.reduce(function (acc, exam) {
+	if(exam.score > 4){
+		return acc + exam.score;
+	}else{
+		return acc;
+	}
+}, 0);
+console.log(approved);
+
+// 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
+
+const media = exams.reduce(function (acc, exam) {
+	
+		return acc + exam.score/exams.length;
+		
+	
+}, 0);
+console.log(media);
